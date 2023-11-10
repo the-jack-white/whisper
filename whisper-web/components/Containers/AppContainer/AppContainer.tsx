1629d0f7cbdce166099ae5022d0e912a4a1db6c3
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2023-11-10 17:51:36 +0000

    v0.1.1 - change screen to 90vh

--- whisper-web/components/Containers/AppContainer/AppContainer.tsx
+++ whisper-web/components/Containers/AppContainer/AppContainer.tsx
@@ -2,7 +2,7 @@ import { ComponentChildren } from "@/types/index.types";
 
 const AppContainer = ({ children }: ComponentChildren) => {
   return (
-    <main className="flex justify-center w-full h-screen p-5 bg-tertiary">
+    <main className="flex justify-center w-full h-[90vh] p-5 bg-tertiary">
       <div className=" bg-secondaryLight w-full h-full rounded-lg">
         {children}
       </div>
