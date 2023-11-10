b2438c220049e0cae7a807bae88de0f369783233
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2023-11-10 17:18:55 +0000

    Remove ChatWindow

--- whisper-web/components/index.ts
+++ whisper-web/components/index.ts
@@ -2,7 +2,6 @@ import Header from "./Header/Header";
 import Navigation from "./Navigation/Navigation";
 import ChatHeader from "./Chat/ChatHeader/ChatHeader";
 import ChatBody from "./Chat/ChatBody/ChatBody";
-import ChatWindow from "./Chat/ChatWindow/ChatWindow";
 import ChatInput from "./Chat/ChatInput/ChatInput";
 import ChatBubble from "./Chat/ChatBubble/ChatBubble";
 import AppContainer from "./Containers/AppContainer/AppContainer";
@@ -19,7 +18,6 @@ export {
   ChatContainer,
   ChatHeader,
   ChatBody,
-  ChatWindow,
   ChatInput,
   ChatBubble,
   Providers,
